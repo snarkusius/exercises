8743e2629d56c2ae0fa0f798d2ababad4178e2e3
snarkusius <marcus@lengquist.se>
2020-10-12 14:45:30 +0200

    uppgift 16

---
+++ A-Beginner/10/script.js
@@ -0,0 +1,37 @@
+let str1 = "supp"
+let str2 = "supp2"
+let str3 = "supp3"
+let alla = str1.concat(str2,str3)
+console.log("körs det här ens")
+
+function fun(neme){
+    console.log(neme)
+    return "supp"
+}
+fun(marcus)
+
+if(true===true){
+    console.log("true")
+}
+
+function pluss(numer1,nummer2,nummer3) {
+    return numer1+nummer2+nummer3
+}
+
+function multiplisera(numer1, numer2) {
+    return numer1 * numer2
+}
+
+function checkAge(age){
+    if(age>65){
+        return "old"
+    }
+    else {
+        return "young"
+    }
+}
+
+
+
+
+
---
+++ A-Beginner/4-9/script.js
@@ -0,0 +1,14 @@
+let objektName = {}
+
+const constname = 5
+
+let name = 6
+name = 8
+
+let arraythings = ["thin","thing2","thing3"]
+
+let marcus = {name:"marcus",lastName:"lengquist",age:18}
+
+//något med ett värde tex
+1+1 1*5 variabe+variabel
+
